7b3b0f38af1c58af33bd9260dfdcd12b3d14b5aa
jaki2012 <jaki2012@sina.cn>
2016-11-25 16:32:00 +0800

    commit before pull

--- src/main/Java/com/tongji409/website/web/HelloController.java
+++ src/main/Java/com/tongji409/website/web/HelloController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController{
 
 
-
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
