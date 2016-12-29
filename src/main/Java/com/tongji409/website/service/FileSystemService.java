90b214afc134a5f304c0019e6108820ed1a22a7d
Novemser <2072806652@qq.com>
2016-12-29 18:36:09 +0800

    modify path

--- src/main/Java/com/tongji409/website/service/FileSystemService.java
+++ src/main/Java/com/tongji409/website/service/FileSystemService.java
@@ -28,7 +28,7 @@ import java.util.zip.ZipInputStream;
 @ComponentScan
 public class FileSystemService extends ServiceSupport {
 
-    private String serverFileRootPath = "/Users/lijiechu/Documents/scode/";
+    private String serverFileRootPath = "F:\\409\\";
 
     /***
      * 把文件从指定URL下载下来
@@ -53,7 +53,7 @@ public class FileSystemService extends ServiceSupport {
 
         fileFolder.mkdirs();
 
-        File saveFile = new File(serverFileRootPath + fileName + "_dir/" + fileName + ".tmp");
+        File saveFile = new File(serverFileRootPath + fileName + "_dir\\" + fileName + ".tmp");
 
         if (saveFile.exists())
             saveFile.delete();
@@ -88,7 +88,7 @@ public class FileSystemService extends ServiceSupport {
 //            offset += count;
 //        }
         // If succeed, change to the desired name
-        File dest = new File(serverFileRootPath + fileName + "_dir/" + fileName + ".zip");
+        File dest = new File(serverFileRootPath + fileName + "_dir\\" + fileName + ".zip");
         if (dest.exists())
             dest.delete();
 
